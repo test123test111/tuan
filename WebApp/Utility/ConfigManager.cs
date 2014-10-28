9ce6694a234cf878ecbe32d2bc7c7bc024f670b9
junyizhang <junyizhang@Ctrip.com>
2014-10-28 18:15:59 +0800

    去掉添加版本号的相关后台代码

--- WebApp/Utility/ConfigManager.cs
+++ WebApp/Utility/ConfigManager.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Configuration;
-using Group.SOARestService.Entity;
 
 namespace Tuan
 {
@@ -16,7 +15,6 @@ namespace Tuan
         /// <returns></returns>
         public static string IncludeStaticFile(string resourcePath)
         {
-            string version = ConfigManager.GetWebresourceVersion();
             string rootPath = ConfigurationManager.AppSettings["WebresourcePDBaseUrl"];
             string debugRootPath = ConfigurationManager.AppSettings["WebresourcePDDebugUrl"];
             string debugModel = System.Web.HttpContext.Current.Request.QueryString["debug"];
@@ -25,24 +23,11 @@ namespace Tuan
                 return String.Format("{0}/{1}", debugRootPath, resourcePath/*, DateTime.Now.Ticks*/);
             }
 #if DEBUG
-            return String.Format("{0}/{1}", debugRootPath, resourcePath + "?v=" + version/*, DateTime.Now.Ticks*/);
+            return String.Format("{0}/{1}", debugRootPath, resourcePath/*, DateTime.Now.Ticks*/);
 #else
-            return String.Format("{0}/{1}", rootPath, resourcePath + '?v=' + version/*, DateTime.Now.Ticks*/);
+            return String.Format("{0}/{1}", rootPath, resourcePath/*, DateTime.Now.Ticks*/);
 #endif
             //return String.Format("{0}/{1}", debugRootPath, resourcePath/*, DateTime.Now.Ticks*/);
-        }
-        public static string GetWebresourceVersion()
-        {
-            try{
-                RestFulServiceClient restFul = RestFulServiceClient.GetInstance();
-                GroupVersionRequestType requestType = new GroupVersionRequestType();
-                GroupVersionResponseType responseType = restFul.GroupVersion(requestType);
-
-                return responseType.releaseno;
-            }catch(Exception e){
-		        return DateTime.Now.ToString("yyyymmdd");
-            }
-
         }
     }
 }
