765e3226825e27dc547baed5655a3998bfbd0bf0
Creemli <li.xx@Ctrip.com>
2014-11-19 14:45:03 +0800

    修复地图导航问题
    Change-Id: I857ccda3b63362e84c85b32d970ef0495c156da1

--- WebApp/GroupH5/views/hotel.map.nav.js
+++ WebApp/GroupH5/views/hotel.map.nav.js
@@ -4,8 +4,8 @@
  * @description 导航页面
  * @since tuan v2.6
  */
-define(['TuanApp', 'libs', 'TuanBaseView', 'cCommonPageFactory', 'text!MapNavTpl'],
-function (TuanApp, libs, TuanBaseView, CommonPageFactory, html) {
+define(['TuanApp', 'libs', 'TuanBaseView', 'cCommonPageFactory', 'cUtility', 'text!MapNavTpl'],
+function (TuanApp, libs, TuanBaseView, CommonPageFactory, Util, html) {
     'use strict';
     var View,
         MAP_KEY = '0b895f63ca21c9e82eb158f46fe7f502',
@@ -22,6 +22,9 @@ function (TuanApp, libs, TuanBaseView, CommonPageFactory, html) {
                 url = _.template(urlTpl, {dest: (p('lng')+','+p('lat')), destName: p('title')});
             this.$el.attr('style', 'height:100%');
             this.$el.html(_.template(this.tpl, {url: url}));
+            
+            Util.isInApp() && this.$el.find('#J_hotelMapIframe').css('top', '0px');
+            
             this.header.set({
                 title: '导航页面',
                 view: this,
