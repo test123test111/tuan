0454ce3e4b7f69641fbb43abe13b894355b7b319
cxw陈旭伟 <xuweichen@ctrip.com>
2014-12-04 18:59:00 +0800

    修改scrollobserver，提高滚动精度
    Change-Id: I9ca699268f5c244eb2f5e33f2dd9740da9098651

--- WebApp/GroupH5/util/scrollobserver.js
+++ WebApp/GroupH5/util/scrollobserver.js
@@ -1,7 +1,9 @@
+var i=0;
 define(['c'], function () {
     var WIN = window,
         DOC = WIN.document,
-        SCROLL_ACCURACY = 1000, //1000 ms
+        SCROLL_ACCURACY = 800, //50 ms
+        SCROLL_TOLERANCE = 0, //滚动方法判断的误差系数，1px
         SCROLL_EVENT = 'scroll',
         SCROLL_STATUS = {
             START: 0,
@@ -20,44 +22,56 @@ define(['c'], function () {
     WIN = $(WIN);
 
     function scrollDirection(x1, x2, y1, y2) {
-        return Math.abs(x1 - x2) >= Math.abs(y1 - y2) ? (x1 - x2 > 0 ? 'left' : 'right') : (y1 - y2 > 0 ? 'up' : 'down');
+        return Math.abs(x1 - x2) >= Math.abs(y1 - y2) ? (x1 - x2 > SCROLL_TOLERANCE ? 'left' : 'right') : (y1 - y2 > SCROLL_TOLERANCE? 'up' : 'down');
+    }
+    function testScrollEnd() {
+        var self = this,
+            body = DOC.body,
+            currentX = body.scrollLeft,
+            currentY = body.scrollTop;
+
+        //if(currentY === lastY && currentX ===  lastX){
+        self.status = SCROLL_STATUS.END;
+        WIN.trigger(SCROLL_EVENTS.END, {
+            direction: self.direction,
+            y: currentY,
+            x: currentX
+        });
+        lastY = body.scrollTop;
+        lastX = body.scrollLeft;
+        // }
     }
     function updateStatus() {
         var self = this,
             body = DOC.body,
             currentX = body.scrollLeft,
             currentY = body.scrollTop;
+        //滚动开始
+        i= new Date().getTime();
 
-        if (this.status == SCROLL_STATUS.START) {
-            this.status = SCROLL_STATUS.BEGIN;
-            this.direction = scrollDirection(lastX, currentX, lastY, currentY);
-            WIN.trigger(SCROLL_EVENTS.START, {
-                direction: self.direction, 
-                y: currentY, 
-                x: currentX
-            });
-        } else {
-            this.status = SCROLL_STATUS.SCROLLING;
-        }
-        clearTimeout(timer);
-        timer = setTimeout(function () {
+        //console.log((self.status+"/"+SCROLL_STATUS.END)+'---'+(typeof self.status)+'/'+(typeof SCROLL_STATUS.END)+'--START----'+(self.status === SCROLL_STATUS.END));
+        if(self.status === SCROLL_STATUS.END){
             self.status = SCROLL_STATUS.START;
-            WIN.trigger(SCROLL_EVENTS.END, {
+
+            self.direction = scrollDirection(lastX, currentX, lastY, currentY);
+            WIN.trigger(SCROLL_EVENTS.START, {
                 direction: self.direction,
                 y: currentY,
                 x: currentX
             });
-            lastY = body.scrollTop;
-            lastX = body.scrollLeft;
-        }, SCROLL_ACCURACY);
+        } else {
+            self.status = SCROLL_STATUS.SCROLLING;
+        }
+        clearTimeout(timer);
+        timer = setTimeout($.proxy(testScrollEnd, self), SCROLL_ACCURACY);
     }
     function scrollHandler() {
-        updateStatus();
-        //trigger('')
+        updateStatus.call(this);
     }
     observer = {
         init: function () {
             this.status = SCROLL_STATUS.END;
+
             this.__scrollHandler = $.proxy(scrollHandler, this);
             return this;
         },
@@ -73,11 +87,11 @@ define(['c'], function () {
 });
 //test code
 /*
-observer.init().enable();
-$(window).on('customScrollStart', function () {
-console.log(arguments);
-})
-$('body').on('click', function () {
-observer.disable();
-})
-*/
+ observer.init().enable();
+ $(window).on('customScrollStart', function () {
+ console.log(arguments);
+ })
+ $('body').on('click', function () {
+ observer.disable();
+ })
+ */
