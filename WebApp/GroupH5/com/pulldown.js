ac9810aa553c720847a9a953f2936d27b44f0089
xuweichen <xuweichen@Ctrip.com>
2014-12-16 23:53:14 +0800

    complete pulldown.js
    Change-Id: I97e9e72615f98ddffe2cf335c98f5ee110c840ad

--- WebApp/GroupH5/com/pulldown.js
+++ WebApp/GroupH5/com/pulldown.js
@@ -6,6 +6,18 @@
 define(['cBase'],function (cBase) {
 
     var PulldownDecorator,
+        CSS3ExpandoPrefix = (function(){
+            var browser = $.browser,
+                prefix = '';
+
+            prefix = browser.webkit ? '-webkit-' :
+                browser.firefox ? '-moz-'   :
+                    browser.ie ? '-ms-'         :
+                        '';
+
+            return prefix;
+
+        })(),
         NOOP = function(){};
 
     function isPullDown(y1, y2) {
@@ -16,35 +28,57 @@ define(['cBase'],function (cBase) {
         initialize: function(view, options){
             this.options = $.extend(this.options, options);
             this.view = view;
-            this._bindTouchEvents();
         },
+        /**
+         * 当滚动条在顶部时才支持下拉
+         * @returns {boolean}
+         */
         ready: function(){
             return !(document.body.scrollTop || document.documentElement.scrollTop);
         },
         __propertys__: function() {
             this.options = {
+                /**
+                 * @cfg {Number} 手指拖动距离与试图移动距离的比例
+                 */
                 offsetRatio: 0.3,
+                /**
+                 * @cfg {int} 拖动到多少位移为有效拖动
+                 */
                 releasePoint: 100,
-                onPullRelease: NOOP,
+                /**
+                 * @event 拖动到开始
+                 */
                 onPullStart: NOOP,
+                /**
+                 * @event 拖动到结束
+                 */
                 onPullEnd: NOOP,
+                /**
+                 * @event 拖动中
+                 */
                 onPulling: NOOP,
+                /**
+                 * @event 拖动到有效点
+                 */
                 onPullMax: NOOP
             };
             this.isPulling = false;
+            this.isMax = false;
         },
-        __lastTouchStartPos: {pageX:0, pageY:0},
+        _lastTouchStartPos: {pageX:0, pageY:0},
         _translate: function(dom, offset, direction){
             direction = direction || 'Y';
 
-            dom.css('transform', 'translate'+direction.toUpperCase()+'('+offset+'px)');
+            dom.css(CSS3ExpandoPrefix+'transform', 'translate'+direction.toUpperCase()+'('+offset+'px)');
         },
         _touchstartHandler: function(e){
             var touches = e.touches[0];
             if(this.ready()){
                 this.isPulling = true;
-                this.__lastTouchStartPos.pageX = touches.pageX;
-                this.__lastTouchStartPos.pageY = touches.pageY;
+                this._lastTouchStartPos.pageX = touches.pageX;
+                this._lastTouchStartPos.pageY = touches.pageY;
+                this.isMax = false;
                 this.options.onPullStart.call(this, e);
             }
             //target.
@@ -53,7 +87,7 @@ define(['cBase'],function (cBase) {
             var target = $(event.currentTarget),
                 touches,
                 pageOffsetY,
-                lastTouch = this.__lastTouchStartPos,
+                lastTouch = this._lastTouchStartPos,
                 options = this.options,
                 offset,
                 maxpull = options.releasePoint;
@@ -75,22 +109,23 @@ define(['cBase'],function (cBase) {
             event.preventDefault();//必须禁止默认行为，否则拖动卡
             offset = (touches.pageY-lastTouch.pageY) * options.offsetRatio;
             if(offset>=maxpull){
-                options.onPullMax.call(this, event, offset);
+                !this.isMax && options.onPullMax.call(this, event, offset, this.isMax);
+                this.isMax = true;
             }else{
-
+                this.isMax = false;
                 options.onPulling.call(this, event, offset);
             }
             (pageOffsetY>=0 || offset<maxpull) && this._translate(target, offset, 'y');
         },
         _touchendHandler: function(e){
             var wrap = this.view.$el;
-
-            this.isPulling = false;
-            wrap.css({
-                'transition': '100ms ease-out'
-            });
-            this._translate(wrap, 0, 'y');
-            this.options.onPullEnd.call(this, e);
+            if(this.isPulling){
+                this.isPulling = false;
+                wrap.css(CSS3ExpandoPrefix+'transition', '100ms ease-out');
+                this._translate(wrap, 0, 'y');
+                this.isMax = false;
+                this.options.onPullEnd.call(this, e);
+            }
         },
         _bindTouchEvents: function(){
             var wrap = this.view.$el,
@@ -110,6 +145,12 @@ define(['cBase'],function (cBase) {
             wrap.off('touchmove', self.__onTouchmoveHandler);
             wrap.off('touchend', self.__onTouchendHandler);
             wrap.off('touchstart', self.__onTouchstartHandler);
+        },
+        disable: function(){
+            this._unbindTouchEvents();
+        },
+        enable: function(){
+            this._bindTouchEvents();
         }
     });
     return PulldownDecorator;
